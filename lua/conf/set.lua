0aadb66b5515cfb1877a8eb9d004913da82777a8
David Garcia Orozco <David.GarOro@protonmail.com>
2024-05-23 22:49:49 -0400

    Begin lua migration

---
+++ lua/conf/set.lua
@@ -0,0 +1,21 @@
+vim.opt.number=true
+vim.opt.relativenumber=true
+
+vim.opt.scrolloff=8
+vim.opt.colorcolumn="80"
+vim.opt.incsearch=true
+
+vim.opt.filetype="on"
+vim.opt.syntax="on"
+vim.opt.wrap=false
+
+vim.opt.shortmess:append({ S = false })
+
+function setTabs(tabsize)
+    vim.opt.tabstop=tabsize
+    vim.opt.softtabstop=tabsize
+    vim.opt.shiftwidth=tabsize
+    vim.opt.expandtab=true
+    vim.opt.autoindent=true
+end
+setTabs(4)
