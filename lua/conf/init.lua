438c890c177073763cab53747813b926d060eb20
David Garcia Orozco <David.GarOro@protonmail.com>
2024-05-26 18:25:22 -0400

    Add lazy plugin manager & telescope

--- lua/conf/init.lua
+++ lua/conf/init.lua
@@ -1,2 +1,3 @@
-require("conf.remap")
 require("conf.set")
+require("conf.remap")
+require("conf.lazy")
---
+++ lua/conf/lazy.lua
@@ -0,0 +1,19 @@
+-- Bootstrap Lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+})
+end
+vim.opt.rtp:prepend(lazypath)
+--
+require("lazy").setup({
+    -- Or tag = 0.1.x
+    {'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' } },
+})
