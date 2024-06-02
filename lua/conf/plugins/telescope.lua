b62ea7bf760a907f088639e5bad347e28efbfe8a
David Garcia Orozco <David.GarOro@protonmail.com>
2024-06-02 03:07:27 -0400

    Add completion stuff

--- lua/conf/plugins/telescope.lua
+++ lua/conf/plugins/telescope.lua
@@ -26,6 +26,7 @@ return {
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
     vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>fc', builtin.grep_string, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
   end,
